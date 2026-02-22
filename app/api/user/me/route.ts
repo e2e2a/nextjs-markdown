e43b56ee883cbe5fb5c8dadfe724465a67a0d117
Reymond Godoy <emonawong22@gmail.com>
2026-02-22 15:11:40 +0800

    integrate get current user in api endpoint

--- app/api/user/me/route.ts
+++ app/api/user/me/route.ts
@@ -1,21 +1,14 @@
 import connectDb from '@/lib/db/connection';
-import { getServerSession } from 'next-auth';
 import { NextResponse } from 'next/server';
-import { authOptions } from '../../auth/[...nextauth]/route';
-import { HttpError } from '@/utils/server/errors';
 import { handleError } from '@/lib/server/handleError';
-import { userRepository } from '@/modules/users/user.repository';
+import { userController } from '@/modules/users/user.controller';
 
 export async function GET() {
   try {
     await connectDb();
-    const session = await getServerSession(authOptions);
-    if (!session || !session.user) throw new HttpError('UNAUTHORIZED');
+    const res = await userController.getCurrentUser();
 
-    const user = await userRepository.findUser(session.user._id, true);
-    if (!user) throw new HttpError('NOT_FOUND', 'No User Found');
-
-    return NextResponse.json(user);
+    return NextResponse.json(res, { status: 200 });
   } catch (err) {
     return handleError(err);
   }

5468e797e7364a1f7192fa4f0d8691b74388def6
Reymond Godoy <emonawong22@gmail.com>
2026-03-01 20:51:34 +0800

    optimized bundler

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,11 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: false,
   devIndicators: false,
+  experimental: {
+    optimizePackageImports: ['lucide-react', '@radix-ui/react-*', '@tanstack/react-table', '@tanstack/react-query', 'recharts'],
+  },
+  transpilePackages: ['yjs', '@hocuspocus/server', '@hocuspocus/transformer'],
+  poweredByHeader: false,
 };
 
 export default nextConfig;

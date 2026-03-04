3e97de9d9f042b901698b784d17e494a8f29c39c
Reymond Godoy <emonawong22@gmail.com>
2026-03-04 16:35:46 +0800

    add

--- app/(users)/projects/[pid]/components/MarkdownSection.tsx
+++ app/(users)/projects/[pid]/components/MarkdownSection.tsx
@@ -127,7 +127,7 @@ export function MarkdownSection({ node, isDirty }: { node: INode; isDirty: boole
   return (
     <>
       <div className="absolute top-12 left-0 right-0 h-1 z-51 w-full bg-background" />
-      <div className="absolute top-13 left-0 right-0 h-14 z-50 flex items-center px-10 border-b border-white/5 bg-sidebar/20 backdrop-blur-xs pointer-events-auto cursor-default drop-shadow-xs shadow-xs">
+      <div className="absolute top-13 left-0 right-0 h-14 z-50 flex items-center px-10 border-b border-white/5 bg-sidebar/80 backdrop-blur-sm pointer-events-auto cursor-default drop-shadow-xs shadow-xs">
         <div className="flex justify-between items-center w-full">
           <h1 className="text-5xl font-bold tracking-tighter text-foreground truncate select-text w-fit! cursor-text">
             {(node as INode)?.title || 'Not Found'}
